9f9fe2abfe3b10fd9fb1cbf5d5f28eedacd0a9f2
Joy <Joy@Joys-MacBook-Pro.local>
2017-03-30 20:17:14 -0400

    added formula

--- main.js
+++ main.js
@@ -16,56 +16,32 @@ $(document).ready(function () { //on DOM ready, bind clicks
 		e.preventDefault(); //disable the button's default behavior
     num1 = parseFloat($("#number1").val()); //store the number of input into num1
     num2 = parseFloat($("#number2").val()); //store the number of input into num2
-    num3 = parseFloat($("number3")).val()); //store the number of input into num3
-    num4 = parseFloat($("number4")).val()); //store the number of input into num4
+    num3 = parseFloat($("#number3").val()); //store the number of input into num3
+    num4 = parseFloat($("#number4").val()); //store the number of input into num4
+		num5 = parseFloat($("#number5").val()); //store the number of input into num5
 
     console.log("The number 1 is :", num1, "The number 2 is:",  num2, "The number 3 is:",  num3, "The number 4 is:",  num4);
 
     //call runMath function
-    runMath();
-
-    //call the showIt function
-    showIt();
+    runMath(num1, num2, num3, num4, num5);
 
     }); // closes mathIt
-    });//closes document ready
+  });//closes document ready
 
 //function that adds, subtracts, multiplies and divides
 
-function runMath(){
-  monthlycostUS = num1 + num2 + num3 + num4 //get monthly cost
-  dailycostUS = monthlycostUS/30; // get difference
-  dailycostIndia = monthlycostUS/  // get costoflivingIndia
-  dailycostChina = monthlycostUS/ // costoflivingChina
-  dailycostJapan = monthlycostUS/ // costoflivingJapan
-  dailycostKenya = monthlycostUS/ // costoflivingKenya
-  dailycostSouthAfrica = monthlycostUS/  //costoflivingSouthAfrica
-  dailycostSenegal = monthlycostUS/ //costoflivingSenegal
-  dailycostFrance = monthlycostUS/ // costoflivingFrance
-  dailycostGermany = monthlycostUS/ // costoflivingGermany
-  dailycostUK = monthlycostUS/ //costoflivingUnitedKingdom
-  dailycostSpain = monthlycostUS/ //costoflivingSpain
-  dailycostMexico = monthlycostUS/ // costoflivingMexico
-  dailycostJamaica = monthlycostUS/ // costoflivingJamaica
-  dailycostBrazil = monthlycostUS/ // costoflivingBrazil
-  dailycostCanada = monthlycostUS/ // costoflivingCanada
-  dailycostAustralia = monthlycostUS/ //costoflivingAustralia
+function runMath(num1, num2, num3, num4, nun5){
+  var USmonthly = num1 + num2 + num3 + num4 + num5; //get monthly cost
+  var USdaily = USmonthly/30;
 
+	console.log("USmonthly", USmonthly);
 
-  // console.log("The sum is", sum);
-  //  console.log("The difference is", difference);
-  // console.log("The product is", product);
-  // console.log("The quotient is", quotient);
-};
-
-//function that will show the reults HTML
-
-function showIt(){
-  $(".first-number").text(num1); // plugs number 1 in text
-  $(".second-number").text(num2) // plugs number 2 in text
-  $("#show-sum").text(sum); // plugs sum into text
-  $("#show-difference").text(difference); // plugs difference into text
-  $("#show-product").text(product); // plugs product into text
-	$("#show-quotient").text(quotient); // plugs quotient into text
-  $('#final-results').addClass('results-show'); //shows the hidden class
-};
+	$("#final-results").find("li").each(function(){
+		var country= $(this);
+		var dailycost = parseFloat(country.data("perdaycost"));
+    var ratio= USdaily/dailycost;
+		var dailyInt= Math.floor(ratio*30);
+		console.log("Country", country, "dc", dailycost, "ratio", ratio, "dailyInt", dailyInt);
+		country.find("span").text(dailyInt);
+	})
+}
